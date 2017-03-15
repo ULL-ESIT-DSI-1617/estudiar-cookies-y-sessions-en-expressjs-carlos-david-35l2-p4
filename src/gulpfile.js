da594587bccff08dec78c7d830ffe9d24576dce7
charly-poket <alu0100844665@ull.edu.es>
2017-03-15 23:58:49 +0100

    Añadiendo gulp file al src

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,1 +1,12 @@
-prueba
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var ghPages = require('gulp-gh-pages');
+
+
+gulp.task('cookies_basic', function(){
+  return gulp.src('./src').pipe(shell(['node /cookies_session_basic/myserver_basic.js']))
+  });
+
+  gulp.task('cookies_middleware', function(){
+      return gulp.src('./src').pipe(shell(['node /cookies_session_middleware/myserver_middleware.js']))
+      });
